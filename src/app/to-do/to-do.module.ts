5b229de68824b0ddb4abb332b646502e310e6593
Ramya <ramya.bala221190@gmail.com>
2024-03-19 22:06:28 +0530

    updated module

--- src/app/to-do/to-do.module.ts
+++ src/app/to-do/to-do.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { ToDoContainerComponent } from './to-do-container/to-do-container.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ToDoDetailComponent } from './to-do-detail/to-do-detail.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { Observable } from 'rxjs';
+import { EnvConfigService } from '../env-config.service';
+
+function appInitialization(envConfigService:EnvConfigService) :()=>Observable<any>{
+  return ()=>envConfigService.loadConfig();
+}
 
 const routes: Routes = [{
   path:"",
@@ -33,6 +39,12 @@ const routes: Routes = [{
     RouterModule.forChild(routes),
   ],
   providers:[
+    {
+      provide:APP_INITIALIZER,
+      useFactory:appInitialization,
+      deps:[EnvConfigService],
+      multi:true
+    }
   ]
 })
 export class ToDoModule { }
