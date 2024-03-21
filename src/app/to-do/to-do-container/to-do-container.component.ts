0482841c9f015be7503bd4552d1d552a99650852
Ramya <ramya.bala221190@gmail.com>
2024-03-21 08:00:02 +0530

    added provider

--- src/app/to-do/to-do-container/to-do-container.component.ts
+++ src/app/to-do/to-do-container/to-do-container.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { ModuleFederationConfigLibService, configModel } from 'module-federation-config-lib';
+import { appName } from '../to-do.module';
 
 @Component({
   selector: 'app-to-do-container',
@@ -7,10 +8,9 @@ import { ModuleFederationConfigLibService, configModel } from 'module-federation
   styleUrls: ['./to-do-container.component.scss']
 })
 export class ToDoContainerComponent {
-  constructor(private envConfigService:ModuleFederationConfigLibService){}
+  constructor(private envConfigService:ModuleFederationConfigLibService, @Inject(appName)public appName:string){}
 
   config:configModel|undefined;
-  appName:string="toDoApp";
 
   ngOnInit(){
     console.log(this.envConfigService.getConfiguration(this.appName))
--- src/app/to-do/to-do.module.ts
+++ src/app/to-do/to-do.module.ts
@@ -1,9 +1,11 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { APP_INITIALIZER, InjectionToken, NgModule } from '@angular/core';
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { ToDoContainerComponent } from './to-do-container/to-do-container.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ToDoDetailComponent } from './to-do-detail/to-do-detail.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+
+export const appName=new InjectionToken("toDoApp");
 
 const routes: Routes = [{
   path:"",
@@ -33,6 +35,10 @@ const routes: Routes = [{
     RouterModule.forChild(routes),
   ],
   providers:[
+    {
+      provide:appName,
+      useValue:"toDoApp"
+    }
   ]
 })
 export class ToDoModule { }
