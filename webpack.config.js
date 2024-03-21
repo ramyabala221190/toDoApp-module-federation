9d3fa8b4670647bcbff1416db16b27d8ef57bd51
Ramya <ramya.bala221190@gmail.com>
2024-03-21 15:23:16 +0530

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
   path.join(__dirname, 'tsconfig.json'),
   [/* mapped paths to share */
-  "module-federation-config-lib"
 ]);
 
 module.exports = {
@@ -48,6 +47,11 @@ module.exports = {
           "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
           "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+          "module-federation-config-lib": {
+            singleton: true,
+            strictVersion: true, 
+            requiredVersion: 'auto'
+        },
           ...sharedMappings.getDescriptors()
         })
         
